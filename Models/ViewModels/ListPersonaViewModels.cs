290a549222bb5c92f91507b2eb472d0a3b0b7ef9
Escobar_Maximiliano <32075931+MaxiEsc@users.noreply.github.com>
2022-02-21 21:54:33 -0300

    Add files via upload
    v2 por cantidad de archivos

---
+++ Models/ViewModels/ListPersonaViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Models.ViewModels
+{
+    public class ListPersonaViewModels
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
---
+++ Models/ViewModels/PersonaViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Models.ViewModels
+{
+    public class PersonaViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(45)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Apellido")]
+        [StringLength(45)]
+        public string Apellido { get; set; }
+        [Required]
+        [Display(Name = "Correo")]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
+        public DateTime Fecha_Nacimiento { get; set; }
+
+    }
+}
